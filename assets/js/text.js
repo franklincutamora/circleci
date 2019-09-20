1b7f406457321441a9c4a766eb198d405dea4307
fcutamora <franklincutamora@gmail.com>
2019-09-20 11:33:41 +0800

    Test

--- assets/js/text.js
+++ assets/js/text.js
@@ -1,1 +1,9 @@
-$(document).leopard();
+// Uncomment this block below to test erroneous javascript.
+
+// $(document).leopard();
+
+// Uncomment this block below to test correct javascript.
+
+(function() {
+    alert('test');
+})();
