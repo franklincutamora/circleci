ac1e11bfe6d88996c90abc2ec2ac168bf9f8be7b
fcutamora <franklincutamora@gmail.com>
2019-09-20 11:55:26 +0800

    javascript linting is now okay

--- assets/js/text.js
+++ assets/js/text.js
@@ -1,8 +1,8 @@
 // Uncomment this block below to test erroneous javascript.
 
-(function(){
-    alter('test');
-})();
+// (function(){
+//     alter('test');
+// })();
 
 // Uncomment this block below to test correct javascript.
 
